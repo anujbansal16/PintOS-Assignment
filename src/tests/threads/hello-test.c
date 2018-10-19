95617db6e050d59888541fb18080e41fcd6c687e
Anuj Bansal <anuj.bansal@students.iiit.ac.in>
2018-10-19 23:35:53 +0530

    Testing Hello.c via run test

---
+++ src/tests/threads/hello-test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "../../threads/hello.h"
+
+void
+test_main1(void)
+{
+  main1();
+}
+
---
+++ src/threads/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "hello.h"
+int main1(){
+	printf("***********hello Pintos*******\n");
+	return 0;
+}
